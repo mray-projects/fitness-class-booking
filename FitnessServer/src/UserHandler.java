45cfe53eb8a18a840a77147d8581e8a5a87f7919
mray-projects <9rayhanh@gmail.com>
2026-09-21 13:17:47 +0400

    This sends registration details to the server.

--- FitnessServer/src/UserHandler.java
+++ FitnessServer/src/UserHandler.java
@@ -1,18 +1,28 @@
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class UserHandler {
 
     public static void handleRegister(HttpExchange exchange) throws IOException {
 
-        String response = "Registration API is working!";
+        InputStream inputStream = exchange.getRequestBody();
+
+        String requestBody = new String(
+                inputStream.readAllBytes(),
+                StandardCharsets.UTF_8
+        );
+
+        System.out.println("Received registration data:");
+        System.out.println(requestBody);
+
+        String response = "Registration received!";
 
         exchange.sendResponseHeaders(200, response.length());
 
-        OutputStream outputStream = exchange.getResponseBody();
-        outputStream.write(response.getBytes());
-        outputStream.close();
+        exchange.getResponseBody().write(response.getBytes());
+        exchange.getResponseBody().close();
     }
 }
